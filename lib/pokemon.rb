955d688a8a1d39756daee84df696eab4b9f84497
github username <github email address>
2018-02-05 17:54:27 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,9 @@
 class Pokemon
 
-  attr_accessor :name, :type, :id, :db
+  attr_accessor :name, :type, :id, :db, :hp
 
   def initialize(args)
     args.each {|key, value| self.send("#{key}=", value)}
-
-
   end
 
   def self.save(name, type, db)
@@ -14,6 +12,15 @@ class Pokemon
 
   def self.find(id, db)
     finder = db.execute("SELECT * FROM pokemon WHERE id= ?;", id)[0]
-    Pokemon.new(id: finder[0], name: finder[1], type:finder[2])
+    Pokemon.new(id: finder[0], name: finder[1], type:finder[2], hp:finder[3])
   end
+
+
+
+  def alter_hp(new_hp, db)
+    db.execute(
+      "UPDATE pokemon SET hp = ? WHERE id = ?",
+      new_hp, self.id)
+  end
+
 end
